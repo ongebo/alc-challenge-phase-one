052537854b098e42952c4753d12bbc5e4b176d71
ongebo <isaacongebo@gmail.com>
2019-07-13 21:24:52 +0300

    Load the ALC homepage using a webview in AboutALCActivity

--- app/src/main/java/com/example/alcchallengeone/AboutALCActivity.kt
+++ app/src/main/java/com/example/alcchallengeone/AboutALCActivity.kt
@@ -2,11 +2,17 @@ package com.example.alcchallengeone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class AboutALCActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_alc)
+
+        val webView: WebView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://andela.com/alc/")
     }
 }
