9311c7ab365760fb63291bcbdce084543daf39c2
ongebo <isaacongebo@gmail.com>
2019-07-13 21:03:15 +0300

    Launch AboutALCActivity when user clicks about button

--- app/src/main/java/com/example/alcchallengeone/MainActivity.kt
+++ app/src/main/java/com/example/alcchallengeone/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.alcchallengeone
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val aboutALCButton: Button = findViewById(R.id.about_button)
+        aboutALCButton.setOnClickListener { showAboutALCScreen() }
+    }
+
+    fun showAboutALCScreen() {
+        val intent = Intent(this, AboutALCActivity::class.java)
+        startActivity(intent)
     }
 }
