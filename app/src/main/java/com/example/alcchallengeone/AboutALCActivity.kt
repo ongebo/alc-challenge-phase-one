3c231c04229e4ddc25d85b7cd5c62fea41f561e1
ongebo <isaacongebo@gmail.com>
2019-07-15 12:30:47 +0300

    Handle SSL error and display progress bar when loading web page

--- app/src/main/java/com/example/alcchallengeone/AboutALCActivity.kt
+++ app/src/main/java/com/example/alcchallengeone/AboutALCActivity.kt
@@ -1,9 +1,14 @@
 package com.example.alcchallengeone
 
+import android.graphics.Bitmap
+import android.net.http.SslError
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.SslErrorHandler
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 
 class AboutALCActivity : AppCompatActivity() {
 
@@ -11,8 +16,25 @@ class AboutALCActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_alc)
 
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+
         val webView: WebView = findViewById(R.id.webview)
-        webView.webViewClient = WebViewClient()
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressBar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.visibility = View.GONE
+            }
+
+            override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+                handler?.proceed() ?: super.onReceivedSslError(view, handler, error)
+            }
+        }
+        webView.settings.javaScriptEnabled = true
         webView.loadUrl("https://andela.com/alc/")
     }
 }
