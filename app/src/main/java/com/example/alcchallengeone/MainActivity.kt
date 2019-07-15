640e793374e31a7e47535369d80eccd6f788fdf4
ongebo <isaacongebo@gmail.com>
2019-07-15 10:02:01 +0300

    Launch the user profile screen from the main activity

--- app/src/main/java/com/example/alcchallengeone/MainActivity.kt
+++ app/src/main/java/com/example/alcchallengeone/MainActivity.kt
@@ -12,11 +12,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val aboutALCButton: Button = findViewById(R.id.about_button)
+        val myProfileButton: Button = findViewById(R.id.profile_button)
         aboutALCButton.setOnClickListener { showAboutALCScreen() }
+        myProfileButton.setOnClickListener { showProfileScreen() }
     }
 
-    fun showAboutALCScreen() {
+    private fun showAboutALCScreen() {
         val intent = Intent(this, AboutALCActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showProfileScreen() {
+        val intent = Intent(this, MyProfileActivity::class.java)
         startActivity(intent)
     }
 }
